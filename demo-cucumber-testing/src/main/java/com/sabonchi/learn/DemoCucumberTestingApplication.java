bd0507773582af8a5275f00db7d981f16bec09e7
Hsabonchi <developersabon@gmail.com>
2024-02-29 21:37:29 -0600

    empty project

---
+++ demo-cucumber-testing/src/main/java/com/sabonchi/learn/DemoCucumberTestingApplication.java
@@ -0,0 +1,13 @@
+package com.sabonchi.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCucumberTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCucumberTestingApplication.class, args);
+	}
+
+}
---
+++ demo-cucumber-testing/src/test/java/com/sabonchi/learn/DemoCucumberTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabonchi.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCucumberTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
